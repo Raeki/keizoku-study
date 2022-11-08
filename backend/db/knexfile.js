686d5c1944a85e488701f80d3a78b6852eb728cf
raeki <mkeithley@gmail.com>
2022-11-08 21:37:06 +0900

    build: initialize knex and create empty migration

---
+++ backend/db/knexfile.js
@@ -0,0 +1,28 @@
+require('dotenv').config();
+
+module.exports = {
+  development: {
+    client: 'postgresql',
+    connection: {
+      database: process.env.PSQL_DATABASE || 'geteatdb',
+      user: process.env.PSQL_USER || 'postgres',
+      password: process.env.PSQL_PASSWORD,
+    },
+    migrations: {
+      directory: './migrations',
+    },
+    seeds: {
+      directory: './seeds',
+    },
+  },
+  production: {
+    client: 'postgresql',
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: './migrations',
+    },
+    seeds: {
+      directory: './seeds',
+    },
+  },
+};
