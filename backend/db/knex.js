751f1dd204eb13627516be2dded10f254d7e4010
raeki <mkeithley@gmail.com>
2022-11-09 20:42:42 +0900

    feat: create initial database migration files

--- backend/db/knex.js
+++ backend/db/knex.js
@@ -1,6 +1,7 @@
-require('dotenv').config();
+require('dotenv').config({ path: '../.env' });
 
 const env = process.env.NODE_ENV || 'development';
+console.log(env);
 
 const config = require('./knexfile');
 const knex = require('knex')(config[env]);
