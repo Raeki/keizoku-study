b4cc061ff610dbe8190e5614bdc04b8e93ad4978
raeki <mkeithley@gmail.com>
2022-11-09 22:05:23 +0900

    feat: initial get /topics

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,17 @@
 require('dotenv').config({ path: '.env.local' });
 const express = require('express');
 const app = express();
+const knex = require('./db/knex');
 const PORT = process.env.PORT || 8080;
-console.log(process.env.PORT);
+
+// console.log(process.env.PORT);
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
+});
+
+app.get('/topics', (req, res) => {
+  res.status(200).send('hello');
 });
 
 app.listen(PORT, () => {
