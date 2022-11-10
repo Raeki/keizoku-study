1edda5012f7ddd623743c27cf16f29e0b4fa583f
raeki <mkeithley@gmail.com>
2022-11-10 19:33:04 +0900

    feat: create allTopics view #9

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,23 @@
 // Dependencies
-import React from 'react';
+import React, { useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
 
 // Components
-import Topics from './components/Topics';
+import AllTopics from './components/AllTopics';
+import Topic from './components/Topic';
 
 export default function App() {
+  // useStates
+  const [topicID, setTopicID] = useState();
+
   return (
     <Routes>
-      <Route exact path='/' element={<Topics />} />
-      <Route path='/topics' element={<Topics />} />
+      <Route exact path='/' element={<AllTopics setTopicID={setTopicID} />} />
+      <Route
+        path='/all-topics'
+        element={<AllTopics setTopicID={setTopicID} />}
+      />
+      <Route path='/topic' element={<Topic topicID={topicID} />} />
     </Routes>
   );
 }
