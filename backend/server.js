1fb8d93bbccfd068b88a4bff86f3eda0915d2ba3
raeki <mkeithley@gmail.com>
2022-11-10 21:04:50 +0900

    API: POST /session

--- backend/server.js
+++ backend/server.js
@@ -14,10 +14,31 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
+// Topics API calls
+
+// GET all topics
 app.get('/topics', async (req, res) => {
   try {
     const data = await knex.select({ id: 'id', name: 'name' }).from('topics');
     console.log('all topics: ' + data);
+    res.status(200).json(data);
+  } catch (e) {
+    console.error(e);
+    res.status(500);
+  }
+});
+
+// Session API calls
+
+// POST a new study session
+app.post('/session', async (req, res) => {
+  try {
+    const { date, time, topicID } = req.body;
+    const data = await knex('sessions').returning(['date', 'time']).insert({
+      date,
+      time,
+      topic_id: topicID,
+    });
     res.status(200).json(data);
   } catch (e) {
     console.error(e);
