8d395c18b11d4e89f9c418c75e2c3eea8bebc209
raeki <mkeithley@gmail.com>
2022-11-10 18:19:46 +0900

    feat: GET /topics

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,16 @@ app.get('/', (req, res) => {
 });
 
 app.get('/topics', (req, res) => {
-  res.status(200).send('hello');
+  (async () => {
+    try {
+      const data = await knex.select({ id: 'id', name: 'name' }).from('topics');
+      console.log('all topics: ' + data);
+      res.status(200).json(data);
+    } catch (e) {
+      console.error(e);
+      res.status(500);
+    }
+  })();
 });
 
 app.listen(PORT, () => {
