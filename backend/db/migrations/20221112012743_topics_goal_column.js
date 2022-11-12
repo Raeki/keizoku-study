37a67dd6e0967940205247226080874882770766
raeki <mkeithley@gmail.com>
2022-11-12 10:30:23 +0900

    feat: add goal column to topics table

---
+++ backend/db/migrations/20221112012743_topics_goal_column.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.alterTable('topics', function (table) {
+    table.integer('goal').unsigned();
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.alterTable('topics', function (table) {
+    table.dropColumn('goal');
+  });
+};
