131969dea9af479ea4f5e9b4bcafff186ede79b8
raeki <mkeithley@gmail.com>
2022-11-15 21:28:07 +0900

    refactor: organize server code

---
+++ backend/paths/sessions.js
@@ -0,0 +1,73 @@
+const knex = require('../db/knex');
+
+async function allSessions(req, res) {
+  const topicID = req.params.topicID;
+  try {
+    const data = await knex('sessions')
+      .where('topic_id', topicID)
+      .select({ id: 'id', date: 'date', time: 'time' });
+    res.status(200).json(data);
+  } catch (e) {
+    console.error(e);
+    res.status(500);
+  }
+}
+
+async function newSession(req, res) {
+  try {
+    const { date, time, topicID } = req.body;
+    const data = await knex('sessions').insert(
+      {
+        date,
+        time,
+        topic_id: topicID,
+      },
+      ['date', 'time', 'id']
+    );
+    res.status(201).json(data[0]);
+  } catch (e) {
+    console.error(e);
+    res.status(500);
+  }
+}
+
+async function editSession(req, res) {
+  try {
+    const { date, time } = req.body;
+    const session_id = req.params.sessionID;
+
+    const data = await knex('sessions').where('id', session_id).update(
+      {
+        date,
+        time,
+      },
+      ['id', 'date', 'time']
+    );
+    console.log(`session: ${session_id} updated to:`);
+    console.log(data);
+    res.status(200).json(data[0]);
+  } catch (e) {
+    console.error(e);
+    res.status(500);
+  }
+}
+
+async function deleteSession(req, res) {
+  try {
+    const session_id = req.params.sessionID;
+
+    const data = await knex('sessions').where('id', session_id).del(['id']);
+    console.log(`session_id: ${data[0].id} deleted`);
+    res.status(200).json(data[0]);
+  } catch (e) {
+    console.error(e);
+    res.status(500);
+  }
+}
+
+module.exports = {
+  allSessions,
+  newSession,
+  editSession,
+  deleteSession,
+};
---
+++ backend/paths/topics.js
@@ -0,0 +1,48 @@
+const knex = require('../db/knex');
+
+async function allTopics(req, res) {
+  try {
+    const data = await knex('topics')
+      .orderBy('name')
+      .select({ id: 'id', name: 'name', goal: 'goal' });
+    res.status(200).json(data);
+  } catch (e) {
+    console.error(e);
+    res.status(500);
+  }
+}
+
+async function newTopic(req, res) {
+  try {
+    const { name } = req.body;
+    const data = await knex('topics').returning(['id', 'name']).insert({
+      name,
+    });
+    console.log('new topic: ');
+    console.log(data[0]);
+    res.status(201).json(data[0]);
+  } catch (e) {
+    console.error(e);
+    res.status(500);
+  }
+}
+
+async function editGoal(req, res) {
+  try {
+    const { goal } = req.body;
+    console.log(goal);
+    const data = await knex('topics')
+      .where('id', req.params.topicID)
+      .update({ goal }, ['goal']);
+    console.log(`topic_id: ${req.params.topicID} new goal: ${goal}`);
+    res.status(200).json(data[0]);
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+module.exports = {
+  allTopics,
+  newTopic,
+  editGoal,
+};
