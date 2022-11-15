0b170179cd2f57d5780b1e2700c5e39e9b60b24b
raeki <mkeithley@gmail.com>
2022-11-15 21:03:54 +0900

    migration: Create user table

---
+++ backend/db/migrations/20221115112127_user_table.js
@@ -0,0 +1,21 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.createTable('users', function (table) {
+    table.increments('id');
+    table.string('username');
+    table.string('token');
+    table.string('password_digest');
+    table.timestamp('created_at');
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.dropTable('users');
+};
