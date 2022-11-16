9d32466dd588fa9ce2e67f3d3c4ef842ce210d6b
raeki <mkeithley@gmail.com>
2022-11-16 18:26:06 +0900

    build: chaneg username to unique email in schema

---
+++ backend/db/migrations/20221116091936_username_to_email.js
@@ -0,0 +1,22 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.alterTable('users', function (table) {
+    table.dropColumn('username');
+    table.string('email');
+    table.unique('email');
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.alterTable('users', function (table) {
+    table.string('username');
+    table.dropColumn('email');
+  });
+};
