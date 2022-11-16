3d77d527ee70e11cc75dcd042c88108671995d40
raeki <mkeithley@gmail.com>
2022-11-16 20:39:59 +0900

    migration: add user_id foreign key to curr. tables

---
+++ backend/db/migrations/20221116113520_topics_user_id.js
@@ -0,0 +1,20 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.alterTable('topics', function (table) {
+    table.integer('user_id');
+    table.foreign('user_id').references('id').inTable('users');
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.alterTable('topics', function (table) {
+    table.dropColumn('user_id');
+  });
+};
